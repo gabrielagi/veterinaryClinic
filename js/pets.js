2d6c5edf6c49b47d187b648a97b77d3b948d5a6a
gabrielagi <gabrielagi503@gmail.com>
2022-07-19 13:43:35 -0300

    first try with events

--- js/pets.js
+++ js/pets.js
@@ -1,3 +1,9 @@
+const tipo = document.getElementById("tipo");
+const nombre = document.getElementById("nombre");
+const dueno = document.getElementById("dueno");
+const indice = document.getElementById("indice");
+const form = document.getElementById("form");
+const btnGuardar = document.getElementById("btn-guardar");
 const listaMascotas = document.getElementById('lista-mascotas');
 
 let mascotas = [
@@ -17,13 +23,38 @@ function listarMascotas() {
     <td>${mascota.dueno}</td>
     <td>
         <div class="btn-group" role="group" aria-label="Basic example">
-            <button type="button" class="btn btn-info editar"><i class="fas fa-edit"></i></button>
+            <button type="button" class="btn btn-info editar onclick="editar(${index})"><i class="fas fa-edit"></i></button>
             <button type="button" class="btn btn-danger eliminar"><i class="far fa-trash-alt"></i></button>
         </div>
     </td>
     </tr>`)
     .join("");
   listaMascotas.innerHTML = htmlMascotas;
+  Array.from(document.getElementsByClassName("editar")).forEach(
+    (botonEditar, index) => (botonEditar.onclick = editar(index))
+  );
+  /*Array.from(document.getElementsByClassName("eliminar")).forEach(
+    (botonEliminar, index) => (botonEliminar.onclick = eliminar(index))
+  );*/
 }
 
+
+function enviarDatos(evento) {
+    evento.preventDefault();
+      const datos = {
+        tipo: tipo.value,
+        nombre: nombre.value,
+        dueno: dueno.value,
+      };
+      mascotas.push(datos);
+      listarMascotas();
+    }
+
+    function editar(indice) {
+        console.log(indice);
+    }
+  
 listarMascotas();
+
+form.onsubmit = enviarDatos;
+btnGuardar.onclick = enviarDatos;
